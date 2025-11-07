e6254dbcd7b068bc8c4eb8882fcca189a64e7604
Lowis <157913738+Lowis-ar@users.noreply.github.com>
2025-11-06 22:29:44 -0600

    Correciones para que funcione en la pagina web :)

--- php/conexion.php
+++ php/conexion.php
@@ -1,12 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "dbtallerb";
+    $host = "sql309.infinityfree.com";
+    $user = "if0_40354505";
+    $pass = "JADB894nH2";
+    $db   = "if0_40354505_dbtallerb";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
 ?>
